c84bd707e3a307a2a81067d470c80d94b85b7c1c
Eric Sayler <esayler@colgate.edu>
2015-03-01 19:49:50 -0500

    implement ARP responder

--- myrouter.py
+++ myrouter.py
@@ -14,28 +14,53 @@ from switchyard.lib.common import *
 class Router(object):
     def __init__(self, net):
         self.net = net
-        # other initialization stuff here
+        self.my_interfaces = net.interfaces()
+        # Interface = (devname, macaddr, ipaddr, netmask) object
 
-
-    def router_main(self):    
+    def router_main(self):
         '''
         Main method for router; we stay in a loop in this method, receiving
         packets until the end of time.
         '''
         while True:
-            gotpkt = True
+            got_packet = True
             try:
-                dev,pkt = self.net.recv_packet(timeout=1.0)
+                device_name,packet = self.net.recv_packet(timeout=1.0)
             except NoPackets:
                 log_debug("No packets available in recv_packet")
-                gotpkt = False
+                got_packet = False
             except Shutdown:
                 log_debug("Got shutdown signal")
                 break
 
-            if gotpkt:
-                log_debug("Got a packet: {}".format(str(pkt)))
+            if got_packet:
+                log_debug("Got a packet: {}".format(str(packet)))
+                if self.arp_for_me(packet):
+                    self.send_arp_reply(device_name, packet)
 
+    def arp_for_me(self, packet):
+        '''
+        '''
+        if packet.get_header(Arp) is not None:
+            arp = packet.get_header(Arp)
+            for interface in self.my_interfaces:
+                if interface.ipaddr == arp.targetprotoaddr:
+                    return True
+        else:
+            return False
+
+    def send_arp_reply(self, device_name, packet):
+        '''
+        '''
+        arp             = packet.get_header(Arp)
+        send_out_device = self.net.interface_by_name(device_name)
+        senderhwaddr    = send_out_device.ethaddr
+        targethwaddr    = arp.senderhwaddr
+        senderprotoaddr = send_out_device.ipaddr
+        targetprotoaddr = arp.senderprotoaddr
+        arp_reply       = create_ip_arp_reply(senderhwaddr, targethwaddr, senderprotoaddr, targetprotoaddr)
+
+        self.net.send_packet(send_out_device.name, arp_reply)
 
 
 def switchy_main(net):
